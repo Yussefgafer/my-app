2505b843ec7ea5190bb10b01380ea893cd846342
Yussefgafer <145143218+Yussefgafer@users.noreply.github.com>
2025-07-10 11:25:22 -0700

    brrrrrra3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-    dependencies {
-        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:2.0.21-1.0.21")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.50")
-    }
-}
-
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    id("com.google.devtools.ksp") version "2.0.21-1.0.21" apply false
-    id("com.google.dagger.hilt.android") version "2.50" apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.hilt) apply false
 }
 
 // Define versions in a single place
 extra.apply {
     set("kotlinVersion", "2.0.21")
-    set("kspVersion", "2.0.21-1.0.21")
-    set("composeCompilerVersion", "1.5.9")
+    set("kspVersion", libs.versions.ksp.get())
+    set("composeCompilerVersion", libs.versions.composeCompiler.get())
 }
