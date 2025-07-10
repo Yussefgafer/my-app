0311067da83dbfe7cdd1fbccee01d72111b5a691
Yussefgafer <145143218+Yussefgafer@users.noreply.github.com>
2025-07-10 14:14:09 -0700

    :/

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,12 +13,6 @@ buildscript {
         google()
         mavenCentral()
     }
-    
-    dependencies {
-        classpath("com.android.tools.build:gradle:${libs.versions.agp.get()}")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${libs.versions.kotlin.get()}")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:${libs.versions.hilt.get()}")
-    }
 }
 
 allprojects {
@@ -26,7 +20,12 @@ allprojects {
         google()
         mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven("https://maven.google.com")
     }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
 
 // Define versions in a single place
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,37 @@
 pluginManagement {
     repositories {
+        gradlePluginPortal()
         google()
         mavenCentral()
-        gradlePluginPortal()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven("https://maven.google.com")
+    }
+    
+    resolutionStrategy {
+        eachPlugin {
+            when (requested.id.namespace) {
+                "com.android" -> useModule("com.android.tools.build:gradle:${requested.version}")
+                "org.jetbrains.kotlin" -> useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${requested.version}")
+                "com.google.dagger.hilt.android" -> useModule("com.google.dagger:hilt-android-gradle-plugin:${requested.version}")
+            }
+        }
     }
 }
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    
     repositories {
         google()
         mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven("https://maven.google.com")
+    }
+    
+    versionCatalogs {
+        create("libs") {
+            from(files("gradle/libs.versions.toml"))
+        }
     }
 }
 
